b55b21d4d2ba29da36adeeac88f5f9bf15b05953
Enzo ANZITE <enzo@kaffein.agency>
2023-02-11 04:48:26 +0100

    view: starting connexion view

---
+++ src/screens/ConnexionView.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+
+class ConnexionView extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Connexion</Text>
+      </View>
+    );
+  }
+}
+
+
+export default ConnexionView;
